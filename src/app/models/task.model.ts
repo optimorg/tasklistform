e162ddc0c493ce928bd2118f6a2d2eb16edec21c
morgREEN <morgreen25@gmail.com>
2023-05-26 19:11:27 -0400

    Added task service crud and model

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,2 +1,4 @@
 export class Task {
+    id?: string;
+    text?: string;
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat';
+import { Task } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private dbPath = '/tasks';
+  
+  tasksRef: AngularFirestoreCollection<Task>;
+  
+  constructor(private db: AngularFirestore) {
+    this.tasksRef = db.collection(this.dbPath); //database collection's path is saved in the tasksRef(reference variable) 
+   }
 
-  constructor() { }
+   getAll(): AngularFirestoreCollection<Task> {
+    return this.tasksRef;
+   }
+
+   create(task: Task): any {
+    return this.tasksRef.add({...task});
+   }
+
+   update(id: string, data:any): Promise<void> {
+    return this.tasksRef.doc(id).update(data);
+   }
+
+   delete(id: string): Promise<void> {
+    return this.tasksRef.doc(id).delete();
+   }
 }
