a67e65df8b903b40c1cd0590484c0fc4692dc84e
morgREEN <morgreen25@gmail.com>
2023-05-26 19:45:52 -0400

    addTask component functionality"

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task.model';
+import { TaskService } from '../services/task.service';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
+  task: Task = new Task();
+  submitted = false;
 
+  constructor(private taskService: TaskService) {}
+  
+  ngOnInit(): void {
+    
+  }
+
+  saveTask(): void {
+    this.taskService.create(this.task).then(() => {  //create the task using the service
+      alert('Created task succesfully!');
+      this.submitted = true;
+    });
+  }
+
+  newTask(): void {
+    this.submitted = false;  //not submitted until saved
+    this.task = new Task();
+  }
 }
