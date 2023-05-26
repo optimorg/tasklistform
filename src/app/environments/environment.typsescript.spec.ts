a97d3b48232e3aa74d0272c8399076719bdb37ee
morgREEN <morgreen25@gmail.com>
2023-05-26 18:46:00 -0400

    Installed ngfire

---
+++ src/app/environments/environment.typsescript.spec.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.typsescript';
+
+describe('Environment', () => {
+  it('should create an instance', () => {
+    expect(new Environment()).toBeTruthy();
+  });
+});
