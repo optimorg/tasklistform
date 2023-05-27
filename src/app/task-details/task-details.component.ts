da2933988024f6e567caeb408a4b7c3fef792f5b
morgREEN <morgreen25@gmail.com>
2023-05-26 20:51:38 -0400

    Added task details functionality in ts

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
+import { TaskService } from '../services/task.service';
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
   styleUrls: ['./task-details.component.css']
 })
-export class TaskDetailsComponent {
+export class TaskDetailsComponent implements OnInit {
+  @Input() task?: Task;
+  @Output() refreshList: EventEmitter<any> = new EventEmitter();
+  currentTask: Task = {
+    id: ''
+    text: ''
+  };
+  message = '';
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.message = '';
+  }
+
+  ngOnChanges(): void {
+    this.message = '';
+    this.currentTask = { ...this.task };
+  }
+
+  updateTask(): void {
+    const data = {
+      text: this.currentTask.text
+    };
+
+    if (this.currentTask.id) {
+      this.taskService.update(this.currentTask.id, data)
+        .then(() => this.message = 'The task was updated successfully!')
+        .catch(err => console.log(err));
+    }
+  }
+
+  deleteTask(): void {
+    if (this.currentTask.id) {
+      this.taskService.delete(this.currentTask.id)
+        .then(() => {
+          this.refreshList.emit();
+          this.message = 'The task was updated successfully!';
+        })
+        .catch(err => console.log(err));
+    }
+  }
 }
